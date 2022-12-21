c2b5d90c304c1d3d9f35bb681a83fc32e636accd
Giuseppe Tribulato <giuseppe.tribulato@here.com>
2022-12-21 23:00:17 +0100

    Fix #13: Fix a bug happening when the central directory file headers are not following the order of the corresponding compressed data. Now the file info records are sorted before use.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name='remotezip',
-    version='0.11.0',
+    version='0.11.1',
     author='Giuseppe Tribulato',
     author_email='gtsystem@gmail.com',
     py_modules=['remotezip'],
