3c797ece8875cc786df8ee1d80886db445a3b83c
Joseph H Kennedy <me@jhkennedy.org>
2023-12-11 12:18:05 -0900

    bump patch part of version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name='remotezip',
-    version='0.12.1',
+    version='0.12.2',
     author='Giuseppe Tribulato',
     author_email='gtsystem@gmail.com',
     py_modules=['remotezip'],
